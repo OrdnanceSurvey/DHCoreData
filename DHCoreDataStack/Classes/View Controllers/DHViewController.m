6432559b03ba5cccc2fbf91ed93b9eefce926fe2
Natalia <osiecka.n@gmail.com>
2018-12-04 20:26:35 +0100

    Xcode 10: Bumps minimum deployment target to iOS 8
    New Xcodes won't create binary for previous versions anyway

--- DHCoreDataStack/Classes/View Controllers/DHViewController.m
+++ DHCoreDataStack/Classes/View Controllers/DHViewController.m
@@ -22,12 +22,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 }
@@ -42,11 +36,6 @@
 
 - (void)viewDidDisappear:(BOOL)animated {
 	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 @end
